a3c942feced018f95fab9c541a1ce773d0deb426
SF_Self-Life-Insurance <ramana838@gmsil.com>
2022-03-03 15:18:56 +0530

    home page changes @ramana

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+//import {homepage} from './home/home.component'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'Self_insurance';
+   constructor(
+    //private homepage:homepage,
+    private router:Router
+    ){}
+  
+  submit(){debugger
+//this.router.navigate(['/homepage']);
+  }
 }
