5896e07f0ce260c7e903e638ddc5bec775bc16de
SF_Self-Life-Insurance <ramana838@gmsil.com>
2022-03-08 12:56:43 +0530

    normal

--- src/app/personal-info/personal-info.component.ts
+++ src/app/personal-info/personal-info.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  //selector: 'app-personal-info',
+  selector: 'app-personal-info',
   templateUrl: './personal-info.component.html',
   styleUrls: ['./personal-info.component.css']
 })
